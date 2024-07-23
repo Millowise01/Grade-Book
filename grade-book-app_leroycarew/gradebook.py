71c07b404fa41600acab981080dc9d1419a9dc7b
Millowise01 <l.carew@alustudent.com>
2024-07-23 04:51:27 -0700

    update

--- grade-book-app_leroycarew/gradebook.py
+++ grade-book-app_leroycarew/gradebook.py
@@ -81,21 +81,4 @@ class GradeBook:
 
     def view_all_courses(self):
         for course in self.course_list:
-            print(f"Name: {course.name}, Trimester: {course.trimester}, Credits: {course.credits}")
-
-    def save_to_file(self, filename):
-        data = {
-            'students': [student.to_dict() for student in self.student_list],
-            'courses': [course.to_dict() for course in self.course_list]
-        }
-        with open(filename, 'w') as f:
-            json.dump(data, f)
-
-    def load_from_file(self, filename):
-        try:
-            with open(filename, 'r') as f:
-                data = json.load(f)
-                self.student_list = [Student.from_dict(s) for s in data['students']]
-                self.course_list = [Course.from_dict(c) for c in data['courses']]
-        except FileNotFoundError:
-            print(f"No data file found at {filename}. Starting with an empty grade book.")
+            print(f"Name: {course.name}, Trimester: {course.trim
